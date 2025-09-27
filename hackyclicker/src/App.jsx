e36f5f5110574d39af7d08d3fcb9a07c8cc2275e
Blake-Berman <blakeberman2005@gmail.com>
2025-09-27 13:50:28 -0400

    UM Yeah
    THisi spretty sick

--- hackyclicker/src/App.jsx
+++ hackyclicker/src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>HackyClicker</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
